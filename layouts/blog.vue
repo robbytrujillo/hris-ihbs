7b6c9fbd4cd73995d0a4b99d75ce0e789db1d20d
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-10-31 12:50:15 +0700

    frontend project

---
+++ layouts/blog.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <div>My blog navigation bar here</div>
+      <Nuxt />
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    layout: 'blog',
+    // OR
+    layout (context) {
+      return 'blog'
+    }
+  }
+  </script>
